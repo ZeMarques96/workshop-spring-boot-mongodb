d740e24b3be22f885165bcda67ab0f9b2c7bf615
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-19 03:33:55 -0300

    findById and exception Handler implementation

--- src/main/java/com/zemarques96/workshopmongo/services/UserService.java
+++ src/main/java/com/zemarques96/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.zemarques96.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zemarques96.workshopmongo.entities.User;
 import com.zemarques96.workshopmongo.repositories.UserRepository;
+import com.zemarques96.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado"));
     }
 }
