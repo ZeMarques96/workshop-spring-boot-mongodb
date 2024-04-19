05420916f1a002543f92f10cb8e0c967c6c94fb2
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-19 02:26:14 -0300

    Entity User and REST

---
+++ src/main/java/com/zemarques96/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.zemarques96.workshopmongo.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zemarques96.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@mail.com");
+        User alex = new User("2", "Alex Green", "alex@mail.com");
+
+        List<User> list = Arrays.asList(maria, alex);
+        return ResponseEntity.ok().body(list);
+    }
+}
