7da5f2a5307ff7eb0eec45cc7263b7cd107e142f
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-22 18:01:36 -0300

    FindById method implemented

---
+++ src/main/java/com/zemarques96/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.zemarques96.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zemarques96.workshopmongo.entities.Post;
+import com.zemarques96.workshopmongo.repositories.PostRepository;
+import com.zemarques96.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+    }
+
+}
