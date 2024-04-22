e6d803057604ef3ac8a410c8a06c427d9129d2b8
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-22 19:10:10 -0300

    Comments on Posts added

--- src/main/java/com/zemarques96/workshopmongo/config/Instantiation.java
+++ src/main/java/com/zemarques96/workshopmongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.zemarques96.workshopmongo.dto.AuthorDTO;
+import com.zemarques96.workshopmongo.dto.CommentDTO;
 import com.zemarques96.workshopmongo.entities.Post;
 import com.zemarques96.workshopmongo.entities.User;
 import com.zemarques96.workshopmongo.repositories.PostRepository;
@@ -40,6 +41,11 @@ public class Instantiation implements CommandLineRunner{
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acorde feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
