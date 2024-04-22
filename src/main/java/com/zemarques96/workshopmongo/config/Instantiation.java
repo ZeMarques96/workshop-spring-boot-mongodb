72406a7203cd9ad07fd5e41ee624181aa9f0de90
José Francisco Marques Neto <netomarques96@gmail.com>
2024-04-22 14:42:45 -0300

    User post reference

--- src/main/java/com/zemarques96/workshopmongo/config/Instantiation.java
+++ src/main/java/com/zemarques96/workshopmongo/config/Instantiation.java
@@ -37,11 +37,14 @@ public class Instantiation implements CommandLineRunner{
         User bob = new User(null , "Bob Grey", "bob@mail.com");
 
         userRepository.saveAll(Arrays.asList(maria,alex, bob));
-        
+
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acorde feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
